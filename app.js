bdfe11a47786e6d4ea7a7faf711c545d18b92ea4
Ruth Gough <ruthegough5@gmail.com>
2021-11-03 09:47:34 -0500

    Including bonus.

--- app.js
+++ app.js
@@ -89,4 +89,28 @@ async function princess(){
   await leia(`Carrie Fisher`);
   leiaName();
 }
-princess()
+princess();
+
+// BONUS
+
+// 4
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('First Promise')
+  }, 2000);
+});
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('Second Promise')
+  }, 3000);
+});
+const p3 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('Third Promise')
+  }, 4000);
+});
+
+// Waits until the longest (Time Wise) Promise is complete before it runs the ".then" (AKA Callback function)
+Promise.all([p1, p2, p3])
+// The value passed in for the "values" parameter are the values returned from each of the Promises still inside an array
+.then((values) => console.log(values));
